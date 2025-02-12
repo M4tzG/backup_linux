1753c0192542a15fc4cd554f154d8836cd79a6f5
Matheus Gabriel <99271709+M4tzG@users.noreply.github.com>
2025-02-12 13:41:06 -0300

    update

--- essentials.sh
+++ essentials.sh
@@ -1,5 +1,6 @@
 # Description: This file contains the essential functions for the installation of Arch Linux
 install_apps() {
+    update_system
     sudo pacman -S firefox kitty fastfetch waybar feh ranger vim neovim obsidian discord qbittorrent krita thermald blueman flatpak python-pillo ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer noto-fonts-cjk adobe-source-han-sans-otc-fonts otf-ipafont tlp
     
 }
@@ -31,12 +32,40 @@ bash_config() {
 
 linux_config() {
     git clone https://github.com/M4tzG/_____Hyprland_____.git
+    cd _____Hyprland_____ && chmod +x install.sh && ./install.sh
 
 }
 
-install_apps
-update_system
-yay_install
-devices
-bash_config
-linux_config
+all_functions() {
+    echo "Executando todas as funções..."
+    install_apps
+    yay_install
+    devices
+    bash_config
+    linux_config
+    echo "Todas as funções foram executadas."
+}
+
+
+# Menu de opções
+echo "Escolha as opções que deseja executar:"
+echo "1 - Instalar aplicativos"
+echo "2 - yay install"
+echo "3 - Devices"
+echo "4 - Bash config"
+echo "5 - Linux config"
+
+read -p "Digite os números separados por espaço: " input
+
+# Processando a entrada do usuário
+for choice in $input; do
+    case $choice in
+        1) install_apps ;;
+        2) yay_install ;;
+        3) devices ;;
+        4) bash_config ;;
+        5) linux_config ;;
+        6) all_functions ;; 
+        *) echo "Opção inválida: $choice" ;;
+    esac
+done
